9a7802c9a451ed6e1068e7a256c43bfa94950e34
manu3618 <manu+github@hbrt.eu>
2020-05-11 15:46:20 +0200

    [PACKAGING] Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="Silver-basson",
+    version="0.0",
+    description="No description available for now",
+    author="manu",
+    author_email="manu+gthub@hbrt.eu",
+    url="https://github.com/manu3618/silver-bassoon.git",
+    package_dir={"": "src"},
+    packages=["basson"],
+)
