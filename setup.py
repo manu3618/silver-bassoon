11a2b2e6f2442fc6b2e15bb7f90c7dfa7f48403c
manu3618 <manu+github@hbrt.eu>
2020-05-11 16:09:34 +0200

    [PACKAGING] Get requirements from *requirements.txt

--- setup.py
+++ setup.py
@@ -2,6 +2,21 @@
 
 from distutils.core import setup
 
+REQUIREMENTS_FILENAMES = ["requirements.txt", "dev-requirements.txt"]
+
+
+def requirements_from_file(filename):
+    with open(filename) as fd:
+        return fd.read().splitlines()
+
+
+def get_requirements(filenames):
+    req = []
+    for filename in filenames:
+        req.extend(requirements_from_file(filename))
+    return req
+
+
 setup(
     name="Silver-basson",
     version="0.0",
@@ -11,4 +26,5 @@ setup(
     url="https://github.com/manu3618/silver-bassoon.git",
     package_dir={"": "src"},
     packages=["bassoon"],
+    requires=get_requirements(REQUIREMENTS_FILENAMES),
 )
